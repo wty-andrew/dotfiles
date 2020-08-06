6a9e4e72867359d1ac45459f761fd783abd56900
Andrew <wty.andrew@gmail.com>
2020-08-06 09:19:19 +0800

    :wrench: change docker install method

--- ubuntu_setup/core/development/tools/docker.sh
+++ ubuntu_setup/core/development/tools/docker.sh
@@ -1,5 +1,11 @@
 install_docker() {
-  sudo snap install docker
+  cd $TEMPORARY_DOWNLOAD_PATH
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+
+  # docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 }
 
 postinstall_docker() {
