9519565ac13f6b62e87a29f64edd360f5982ec95
Andrew <wty.andrew@gmail.com>
2020-12-13 10:44:12 +0800

    :wrench: remove zsh-autocomplete

--- ubuntu_setup/customize/zsh.sh
+++ ubuntu_setup/customize/zsh.sh
@@ -4,7 +4,6 @@ install_oh_my_zsh() {
 
 install_zsh_configuration() {
   git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-  git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
   cp $FILES_DIR/bullet-train.zsh-theme ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes
 }
