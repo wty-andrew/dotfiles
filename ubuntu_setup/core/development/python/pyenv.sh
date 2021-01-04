77304effa8fbf515958d972e77ca262db6336abc
Andrew <wty.andrew@gmail.com>
2021-01-04 14:43:03 +0800

    :wrench: add pyenv register system python

--- ubuntu_setup/core/development/python/pyenv.sh
+++ ubuntu_setup/core/development/python/pyenv.sh
@@ -19,4 +19,9 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 EOF
 )
   write_to_local_config "$header" "$text"
+
+  # register system python (taken from http://github.com/doloopwhile/pyenv-register)
+  sudo apt install -y python3-virtualenv
+  local venv=$(/usr/bin/python3 -c 'import sys; print("system-{}".format(".".join(map(str, sys.version_info[:3]))))')
+  /usr/bin/python3 -m virtualenv -p /usr/bin/python3 --system-site-packages $HOME/.pyenv/versions/$venv
 }
