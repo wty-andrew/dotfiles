8251df9c0c7f39a3388318786b50e41980efb143
Andrew <wty.andrew@gmail.com>
2021-01-07 09:37:08 +0800

    :sparkles: add a few shell alias/function

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,4 +1,5 @@
 # github.com/alexmurray/emacs-snap/issues/10
 alias spacemacs="(/snap/bin/emacs $* 2>&1 1>/dev/null | grep -v Fontconfig | grep -v libgvfsdbus.so &)"
 alias bat=batcat
-
+alias copy="xclip -selection clipboard"
+alias ports="netstat -ntlp"
--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,5 +1,5 @@
 function mkd() {
-    mkdir -p "$@" && cd "$_"
+  mkdir -p "$@" && cd "$_"
 }
 
 function mkf() {
@@ -15,4 +15,16 @@ function myip() {
   do
     echo $ip
   done
+}
+
+function reset-permissions() {
+  if [[ $# -eq 0 ]] ; then
+    echo "usage: reset-permissions <path>"
+    return 1
+  elif [[ -d $1 ]] ; then
+    find $1 -type d -print0 | xargs -0 chmod 0775
+    find $1 -type f -print0 | xargs -0 chmod 0664
+  else
+    chmod 664 $1
+  fi
 }
