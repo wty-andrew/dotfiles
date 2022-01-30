7572de4238377d71d7e2903a1154dd38abcf2c4e
Andrew <wty.andrew@gmail.com>
2022-01-30 12:10:06 +0800

    :wrench: add function to enable huion shortkeys

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -28,3 +28,22 @@ function reset-permissions() {
     chmod 664 $1
   fi
 }
+
+function huion() {
+  case $1 in
+    krita)
+      xsetwacom set "HID 256c:006d Pad pad" Button 1 "key +ctrl +z -z -ctrl" # undo
+      xsetwacom set "HID 256c:006d Pad pad" Button 2 "key +ctrl +shift +z -z -ctrl -shift" # redo
+      xsetwacom set "HID 256c:006d Pad pad" Button 3 "key e" # toggle eraser mode
+      xsetwacom set "HID 256c:006d Pad pad" Button 8 "key ctrl"
+      xsetwacom set "HID 256c:006d Pad pad" Button 9 "key shift"
+      xsetwacom set "HID 256c:006d Pad pad" Button 10 "key alt"
+      ;;
+    blender)
+      # TODO: 
+      ;;
+    *)
+      echo "usage: huion [krita|blender]"
+      return 1
+  esac
+}
