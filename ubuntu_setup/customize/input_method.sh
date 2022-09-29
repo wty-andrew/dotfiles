56b379857e8cc05f0d20c8f4eae7bca0712c6cb2
Andrew <wty.andrew@gmail.com>
2022-09-29 21:47:03 +0800

    :bug: fix fcitx copy location

--- ubuntu_setup/customize/input_method.sh
+++ ubuntu_setup/customize/input_method.sh
@@ -1,6 +1,6 @@
 install_fcitx_configuration() {
-  mkdir -p $HOME/.config/fcitx
-  sudo cp $FILES_DIR/fcitx/boshiamy.mb /usr/share/fcitx/table
+  mkdir -p $HOME/.config/fcitx/table
+  cp $FILES_DIR/fcitx/boshiamy.mb $HOME/.config/fcitx/table
   cp $FILES_DIR/fcitx/config $HOME/.config/fcitx
 }
 
