0ccd6bb8b96589880e696b596c736e5e31f6613d
Andrew <wty.andrew@gmail.com>
2022-10-30 19:33:00 +0800

    :wrench: set zathura as default pdf reader

--- ubuntu_setup/core/programs/reader/zathura.sh
+++ ubuntu_setup/core/programs/reader/zathura.sh
@@ -1,3 +1,7 @@
 install_zathura() {
   sudo apt install -y zathura zathura-pdf-poppler zathura-djvu zathura-ps
 }
+
+postinstall_zathura() {
+  xdg-mime default org.pwmt.zathura.desktop application/pdf
+}
