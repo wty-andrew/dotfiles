879ec6ae707bbaced15e8871b203c8e10273f7b3
Andrew <wty.andrew@gmail.com>
2024-09-11 22:05:50 +0800

    :wrench: Upgrade docker to version 27 (default)

--- system/virtualisation/docker.nix
+++ system/virtualisation/docker.nix
@@ -1,7 +1,6 @@
-{ username, pkgs, ... }: {
+{ username, ... }: {
   virtualisation.docker = {
     enable = true;
-    package = pkgs.docker_26;
   };
 
   users.users.${username}.extraGroups = [ "docker" ];
