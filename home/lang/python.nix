5dffa79f7e73b816e0da515068b891014fb4369b
Andrew <wty.andrew@gmail.com>
2024-10-22 16:42:18 +0800

    :wrench: Remove pyenv in favor of uv

--- home/lang/python.nix
+++ home/lang/python.nix
@@ -1,8 +1,4 @@
-{ config, pkgs, ... }:
-let
-  pyenvRoot = "${config.home.homeDirectory}/.pyenv";
-in
-{
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     (python3.withPackages (ps: with ps; [
       argcomplete
@@ -14,31 +10,9 @@ in
     (poetry.withPlugins (ps: with ps; [
       poetry-plugin-up
     ]))
-    pyenv
     ruff
     uv
   ];
 
-  home.sessionVariables = {
-    PYENV_ROOT = pyenvRoot;
-  };
-
-  home.sessionPath = [
-    "${pyenvRoot}/bin"
-  ];
-
-  home.file.".pyenv/plugins/pyenv-virtualenv".source = pkgs.fetchFromGitHub {
-    owner = "pyenv";
-    repo = "pyenv-virtualenv";
-    rev = "37917069ecba16602decd3dd2c8b09121c673a41";
-    sha256 = "AnHU7BSERnTWV7lTvfloptCk4flPvGGbm1GGmju4OnU=";
-  };
-
   xdg.configFile."ruff".source = ../../config/ruff;
-
-  programs.zsh.initExtra = ''
-    eval "$(pyenv init --path)"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-  '';
 }
