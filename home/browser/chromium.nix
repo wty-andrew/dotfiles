6cb207cd64e763c2ce73b080ac06a33a84ae08a2
Andrew <wty.andrew@gmail.com>
2024-11-03 21:15:07 +0800

    :wrench: Patch chromium desktop file for ime

--- home/browser/chromium.nix
+++ home/browser/chromium.nix
@@ -1,3 +1,7 @@
-_: {
+{ pkgs, helpers, ... }: {
   programs.chromium.enable = true;
+
+  home.packages = with pkgs; [
+    (helpers.patchDesktopEntry chromium "chromium-browser" "^Exec=chromium" "Exec=chromium --enable-wayland-ime")
+  ];
 }
