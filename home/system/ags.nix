f1c56de2a9b1cf801cf455735498f995f7b7674a
Andrew <wty.andrew@gmail.com>
2024-11-14 10:46:19 +0800

    :arrow_up: Regular upgrade dependencies

--- home/system/ags.nix
+++ home/system/ags.nix
@@ -1,16 +1,34 @@
-{ inputs, config, pkgs, helpers, ... }: {
+{ inputs, config, pkgs, helpers, ... }:
+let
+  agsPackages = with inputs.ags.packages.${pkgs.system}; [
+    apps
+    battery
+    bluetooth
+    hyprland
+    mpris
+    network
+    notifd
+    tray
+    wireplumber
+  ];
+in
+{
   imports = [ inputs.ags.homeManagerModules.default ];
 
   home.packages = with pkgs; [
+    inputs.ags.packages.${system}.io # astal cli
     sassc
-  ];
+  ] ++ agsPackages;
 
   programs.ags = {
     enable = true;
 
+    # TODO: ExecStart should be "ags run" ?
+    # systemd.enable = true;
+
     extraPackages = with pkgs; [
       libsoup_3
-    ];
+    ] ++ agsPackages;
   };
 
   xdg.configFile."ags".source = config.lib.file.mkOutOfStoreSymlink (helpers.runtimePath ../../config/ags);
