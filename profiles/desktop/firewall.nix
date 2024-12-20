2219d30638af01c1df43351565fb515e812c12ca
Andrew <wty.andrew@gmail.com>
2024-12-20 14:45:26 +0800

    :wrench: Add firewall rule for multicast address

--- profiles/desktop/firewall.nix
+++ profiles/desktop/firewall.nix
@@ -1,6 +1,15 @@
 _: {
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 80 443 ];
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 443 ];
+
+      extraInputRules = ''
+        ip daddr 224.0.0.0/4 udp dport 0-65535 accept
+        ip saddr 224.0.0.0/4 udp sport 0-65535 accept
+      '';
+    };
+
+    nftables.enable = true;
   };
 }
