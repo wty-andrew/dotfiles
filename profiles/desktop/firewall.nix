1a65cd324e5a19ff5f62c1a96c2b77c4bce3f671
Andrew <wty.andrew@gmail.com>
2025-07-10 09:57:38 +0800

    :wrench: Update desktop network settings

--- profiles/desktop/firewall.nix
+++ profiles/desktop/firewall.nix
@@ -2,7 +2,14 @@ _: {
   networking = {
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 80 443 ];
+      allowedTCPPorts = [
+        80   # http
+        443  # https
+        6443 # k3s
+        3000 # web development
+        8000 # web development
+        7447 # zenoh
+      ];
 
       extraInputRules = ''
         ip daddr 224.0.0.0/4 udp dport 0-65535 accept
