175d0683d489776d26eee65c647a160d85f6cf70
Andrew <wty.andrew@gmail.com>
2025-08-27 22:10:04 +0800

    :wrench: Update nix-ld libraries

--- profiles/base/configuration.nix
+++ profiles/base/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, username, hostname, ... }: {
+{ options, pkgs, username, hostname, ... }: {
   boot.loader = {
     efi.canTouchEfiVariables = true;
 
@@ -41,5 +41,20 @@
     options = "--delete-older-than 14d";
   };
 
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    # from: https://discourse.nixos.org/t/pyhton-use-pyqt6-within-an-virtual-environment-venv/66494
+    libraries = options.programs.nix-ld.libraries.default ++ (
+      with pkgs; [
+        dbus # libdbus-1.so.3
+        fontconfig # libfontconfig.so.1
+        freetype # libfreetype.so.6
+        glib # libglib-2.0.so.0
+        libGL # libGL.so.1
+        libxkbcommon # libxkbcommon.so.0
+        xorg.libX11 # libX11.so.6
+        wayland
+      ]
+    );
+  };
 }
