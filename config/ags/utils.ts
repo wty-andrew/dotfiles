0178ef1b75fed14e498fb777c007dac77526656b
Andrew <wty.andrew@gmail.com>
2025-11-29 20:49:29 +0800

    :wrench: Switch to hyprpanel

--- config/ags/utils.ts
+++
@@ -1,8 +0,0 @@
-export const range = (startOrStop: number, stop?: number) => {
-  const start = stop === undefined ? 0 : startOrStop
-  const end = stop || startOrStop
-  return [...Array(end - start)].map((_, i) => i + start)
-}
-
-export const clamp = (value: number, lower: number, upper: number) =>
-  Math.max(lower, Math.min(upper, value))
