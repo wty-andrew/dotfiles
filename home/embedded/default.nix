3e4f7e256c8a69972da3e62d73c804dcc4eb7660
Andrew <wty.andrew@gmail.com>
2026-03-26 23:39:23 +0800

    :wrench: Add pico tools

--- home/embedded/default.nix
+++ home/embedded/default.nix
@@ -1,6 +1,7 @@
 { ... }: {
   imports = [
     ./esp32.nix
+    ./pico.nix
     ./platformio.nix
     ./probe-rs.nix
     ./stlink.nix
--- system/drivers/udev.nix
+++ system/drivers/udev.nix
@@ -4,4 +4,33 @@
     platformio-core
     stlink
   ];
+
+  # copied from https://github.com/raspberrypi/picotool/blob/master/udev/60-picotool.rules, better
+  # to have picotool derivation to install the rules
+  services.udev.extraRules = ''
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="0003", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="0009", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="000a", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="000f", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+  '';
 }
