be66480aaf259cf64b34f41bd8d428be8b8ee3ac
Andrew <wty.andrew@gmail.com>
2026-07-22 14:50:32 +0800

    :wrench: Add minimal regreet config

--- system/desktop-environment/regreet.nix
+++ system/desktop-environment/regreet.nix
@@ -1,5 +1,33 @@
-_: {
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    regreet
+  ];
+
   programs.regreet = {
     enable = true;
+
+    settings = {
+      skip_selection = true;
+    };
+
+    theme =
+      let
+        flavor = "frappe";
+        accent = "mauve";
+        size = "compact";
+        tweak = "rimless";
+      in
+      {
+
+        name = "catppuccin-${flavor}-${accent}-${size}+${tweak}";
+        package = pkgs.catppuccin-gtk.override {
+          inherit size;
+          accents = [ accent ];
+          tweaks = [ tweak ];
+          variant = flavor;
+        };
+      };
+
+    extraCss = ./../../config/regreet/regreet.css;
   };
 }
